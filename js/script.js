8f608804714bde9353af9713da3b3a43b9bcdc80
note\다혜 <ydh0606@naver.com>
2018-06-21 07:31:18 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 // Mouse Pointer
-
 var $cursor = document.querySelector('#cursor');
 var $allA = document.querySelectorAll('a');
 
 
 
 function mousePointer(e){
-
     //console.log('Mouse Pointer');
-
     $cursor.style.transform = 'translate(' + e.pageX + 'px, ' + e.pageY + 'px)';
-   
 }
 
 window.addEventListener('mousemove', mousePointer);
-
-
 
 function aPointer01(e){
    $cursor.style.backgroundColor = 'rgba(0,0,0,0.5)';
@@ -26,10 +20,8 @@ function aPointer02(e){
 }
 
 for(var i = 0; i < $allA.length; i++){
-
    $allA[i].addEventListener('mouseover', aPointer01);
    $allA[i].addEventListener('mouseout', aPointer02);
-
 }
 
 
@@ -43,32 +35,27 @@ for(var i = 0; i < $allA.length; i++){
 
 // Menu Button
 
-var $navBtn = document.querySelector('#nav_btn');
+var $menuBtn = document.querySelector('#menu_btn');
 var $navBg = document.querySelector('#nav_bg');
-var $navSub = document.querySelectorAll('.menu_sub');
+var $navSub = document.querySelectorAll('.sub_menu_title');
 
 var _bgH = 250;
 
 
 
 function onNav(){
-
-    //console.log('Mouse Over Nav Btn');
-
+    //console.log('MouseOver Menu Btn');
     $navBg.style.height = _bgH + 'px';
-
 }
 
 function outNav(){
-
-    //console.log('Mouse Out Nav Btn');
-
+    //console.log('MouseOut Menu Btn');
     $navBg.style.height = 0 + 'px';
-
 }
 
-$navBtn.addEventListener('mouseover', onNav);
-$navBtn.addEventListener('mouseout', outNav);
+$menuBtn.addEventListener('mouseover', onNav);
+$menuBtn.addEventListener('mouseout', outNav);
+
 $navBg.addEventListener('mouseover', onNav);
 $navBg.addEventListener('mouseout', outNav);
 
@@ -78,36 +65,14 @@ $navBg.addEventListener('mouseout', outNav);
 
 
 
-      // if(!$el.classList.contains('circle')) {
-      //     console.log('Trailer Text');
-
-      //     $tText.style.display = 'block';
-      //     $tCircle.style.display = 'none';
-
-      //     //$trailer.addEventListener('mouseover', onText);
-      //     //$trailer.addEventListener('mouseout', outText);
-
-      // } else {
-      //     $tText.style.display = 'none';
-      //     $tCircle.style.display = 'block';
-      // }
 
 
 
-
-
-
-
-
-
-
-
-
-// Menu Tab
+// Nav Tab
 
 var $menuTitle = document.querySelectorAll('.menu_title');
 
-var $mainWrap = document.querySelector('#main_wrap');
+var $wrapper = document.querySelector('#wrapper');
 var $contentBox = document.querySelectorAll('.bg');
 
 var $tText = document.querySelector('#t_text');
@@ -121,30 +86,29 @@ var _exId = _cuId;
 function menuClick(id){
 
     function onClickMenu(event){
-      event.preventDefault();
-
       //console.log('select menu');
+      event.preventDefault();
+      
 
       var $el = this;
 
       if(!$el.classList.contains('menu_select')){ 
-
           _cuId = id;
 
           $menuTitle[_exId].classList.remove ('menu_select');
           $el.classList.add ('menu_select');
 
-          $contentBox[_exId].classList.remove('fade_in', 'text_index');
-          $contentBox[_cuId].classList.add('fade_in', 'text_index');
+          $contentBox[_exId].classList.remove ('fade_in', 'text_index');
+          $contentBox[_cuId].classList.add ('fade_in', 'text_index');
 
-          $mainWrap.style.background = 'url(img/bg_' + _exId + '.jpg) no-repeat center center'
-          $mainWrap.style.backgroundSize = 'cover'
+          $wrapper.style.background = 'url(img/bg_' + _exId + '.jpg) no-repeat center center'
+          $wrapper.style.backgroundSize = 'cover'
 
           _exId = _cuId;
       }
 
       if(!$el.classList.contains('circle')) {
-          console.log('Trailer Text');
+          //console.log('Trailer Text');
 
           $tText.style.display = 'block';
           $tCircle.style.display = 'none';
@@ -156,7 +120,6 @@ function menuClick(id){
           $tText.style.display = 'none';
           $tCircle.style.display = 'block';
       }
-
     }
 
     $menuTitle[id].addEventListener('click', onClickMenu);
@@ -165,7 +128,6 @@ function menuClick(id){
 
 
 function selectMenu(){
-
     for(var i = 0; i < $menuTitle.length; i++){
         menuClick(i);
     }
@@ -182,7 +144,7 @@ selectMenu ();
 
 
 
-// Trailer Box
+// Trailer
 
 var $tText = document.querySelector('#t_text');
 var $tCircle = document.querySelector('#t_circle');
@@ -218,21 +180,17 @@ var $tPlay = document.querySelector('#t_play');
 
 
 function onTrailer(){
-
     //console.log('onTrailer');
 
     $tImage.src = 'img/t_video.gif';
     $tPlay.style.transform = 'scale(0, 0)';
-   
 }
 
 function outTrailer(){
-
     //console.log('outTrailer');
 
     $tImage.src = 'img/t_img.jpg';
     $tPlay.style.transform = 'scale(1, 1)';
-   
 }
 
 $trailer.addEventListener('mouseover', onTrailer);
@@ -251,7 +209,7 @@ $trailer.addEventListener('mouseout', outTrailer);
 
 var $parallax = document.querySelectorAll('.parallax');
 
-var $mainColor = document.querySelector('.main_color');
+var $homeColor = document.querySelector('.home_color');
 var $colorBg = document.querySelectorAll('.color_bg');
 
 var screenW = window.innerWidth;
@@ -263,7 +221,6 @@ var _tNum = 5;
 
 
 function mouseParallax(e){
-
     //console.log('Mouse Parallax');
 
    for(var i = 0; i < $parallax.length; i++){
@@ -271,11 +228,9 @@ function mouseParallax(e){
     }
 
     //$trailer.style.transform ='translate(' + e.pageX/screenH * _tNum + 'px, ' + e.pageY/screenW * _tNum + 'px)';
-   
 }
 
 function mouseGradient(e){
-
     //console.log('Mouse Gradient');
 
     var mouseX = Math.round(event.pageX / screenW * 100);
@@ -284,12 +239,7 @@ function mouseGradient(e){
    for(var i = 0; i < $colorBg.length; i++){
         $colorBg[i].style.background = 'radial-gradient(at ' + mouseX + '% ' + mouseY + '%, #2b4355, #c56b15)';
     }
-   
 }
 
 window.addEventListener('mousemove', mouseParallax);
 window.addEventListener('mousemove', mouseGradient);
-
-
-
-
